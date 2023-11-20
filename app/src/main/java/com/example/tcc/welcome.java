fe548b7567229a601552ee9d49164f7a1203dbdf
eduardo.fsl <eduardo.fsl@sempreceub.com>
2023-11-20 20:14:54 -0300

    Comit4

--- app/src/main/java/com/example/tcc/welcome.java
+++ app/src/main/java/com/example/tcc/welcome.java
@@ -5,22 +5,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-public class inicioquatro extends AppCompatActivity {
+public class welcome extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inicioquatro);
+        setContentView(R.layout.activity_welcome);
 
         // Encontra o botão "Ir para a próxima página"
-        Button button = findViewById(R.id.btn_4);
+        Button button = findViewById(R.id.btn_5);
 
         // Define um ouvinte de clique no botão
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // Cria um Intent com a atividade de destino definida para NextActivity.class
-                Intent intent = new Intent(inicioquatro.this, welcome.class);
+                Intent intent = new Intent(welcome.this, cadastro.class);
 
                 // Chama o método startActivity() para iniciar a NextActivity
                 startActivity(intent);
