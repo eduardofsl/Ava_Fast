590e6b32d89aad67e014991ad563c3bc086255fd
eduardo.fsl <eduardo.fsl@sempreceub.com>
2023-11-21 16:23:10 -0300

    login com google

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    viewBinding{
+        enable = true
+    }
+
 }
 
 dependencies {
@@ -36,6 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
